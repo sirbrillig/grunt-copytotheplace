ed04c0526efc8c85ffb773996ec7e1f23f9108d2
Payton Swick <payton@foolord.com>
2016-02-18 14:47:33 -0500

    Add tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,6 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
-        '<%= nodeunit.tests %>'
       ],
       options: {
         jshintrc: '.jshintrc'
@@ -28,31 +27,21 @@ module.exports = function(grunt) {
       tests: ['tmp']
     },
 
-    // Configuration to be run (and then tested).
-    copytotheplace: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
+    env: {
+      doCopy: {
+        COPYTOTHEPLACE: 'tmp'
       },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
     },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+    // Configuration to be run (and then tested).
+    copytotheplace: {
+      doCopy: ['test/fixtures/copyplease'],
+      noCopy: ['test/fixtures/donotcopy']
+    },
 
+    simplemocha: {
+      src: ['test/copytotheplace_test.js']
+    }
   });
 
   // Actually load this plugin's task(s).
@@ -61,11 +50,12 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-simple-mocha');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'copytotheplace', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'copytotheplace:noCopy', 'env:doCopy', 'copytotheplace:doCopy', 'simplemocha']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- test/copytotheplace_test.js
+++ test/copytotheplace_test.js
@@ -1,48 +1,18 @@
 'use strict';
 
 var grunt = require('grunt');
+var chai = require('chai');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+var expect = chai.expect;
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+describe('copytotheplace', function() {
+ it('does not copy any files if the COPYTOTHEPLACE env variable is not set', function() {
+   expect(grunt.file.exists('tmp/donotcopy')).to.not.be.ok;
+ });
 
-exports.copytotheplace = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  },
-  default_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
-
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
-};
+ it('copies the files to the location in the COPYTOTHEPLACE env variable', function() {
+   var actual = grunt.file.read('tmp/copyplease');
+   var expected = grunt.file.read('test/fixtures/copyplease');
+   expect(actual).to.equal(expected);
+ });
+});
